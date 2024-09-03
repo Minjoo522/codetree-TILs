96a0c3cf2581c13712f0ce75d99ef15ab4fe9fc9
Kim Minjoo(김민주/리브) <131349867+Minjoo522@users.noreply.github.com>
2024-09-03 23:20:20 +0900

    Title: 계단 오르기, Time: 127ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240903/계단 오르기/climbing-stairs.py
+++ 240903/계단 오르기/climbing-stairs.py
@@ -8,9 +8,8 @@ dp = [0] * (n + 4)
 dp[1] = 0
 dp[2] = 1
 dp[3] = 1
-dp[4] = 1
 
 for i in range(5, n + 1):
-    dp[i] = dp[i - 1] + dp[i - 2]
+    dp[i] = dp[i - 2] + dp[i - 3]
 
 print(dp[n] % 10007)

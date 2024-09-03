d88ee4fac7f1148d79b6c90de4b7697e55f2c211
Kim Minjoo(김민주/리브) <131349867+Minjoo522@users.noreply.github.com>
2024-09-03 23:22:26 +0900

    Title: 계단 오르기, Time: 127ms, Memory: 24MB, Status: Passed - CodeTree

--- 240903/계단 오르기/climbing-stairs.py
+++ 240903/계단 오르기/climbing-stairs.py
@@ -4,12 +4,12 @@ input = sys.stdin.readline
 
 n = int(input())
 
-dp = [0] * (n + 4)
+dp = [0] * 1000
 dp[1] = 0
 dp[2] = 1
 dp[3] = 1
 
 for i in range(4, n + 1):
-    dp[i] = dp[i - 2] + dp[i - 3] % 10007
+    dp[i] = (dp[i - 2] + dp[i - 3]) % 10007
 
 print(dp[n])

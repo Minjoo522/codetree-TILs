5f920bcd83769263b5558a550e8c4bd8fbe90c3d
Kim Minjoo(김민주/리브) <131349867+Minjoo522@users.noreply.github.com>
2024-09-03 23:31:52 +0900

    Title: 사각형 채우기, Time: 141ms, Memory: 24MB, Status: Passed - CodeTree

--- 240903/사각형 채우기/rectangle-fill.py
+++ 240903/사각형 채우기/rectangle-fill.py
@@ -4,10 +4,10 @@ input = sys.stdin.readline
 
 n = int(input())
 dp = [0] * 1000
+dp[0] = 1  # 아예 놓지 않는 경우
 dp[1] = 1
-dp[2] = 2
 
-for i in range(3, n + 1):
+for i in range(2, n + 1):
     dp[i] = (dp[i - 1] + dp[i - 2]) % 10007
 
 print(dp[n])

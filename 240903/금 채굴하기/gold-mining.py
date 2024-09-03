0407b6788913abe81057f574db4350ea02c8808b
Kim Minjoo(김민주/리브) <131349867+Minjoo522@users.noreply.github.com>
2024-09-03 17:41:03 +0900

    Title: 금 채굴하기, Time: 492ms, Memory: 27MB, Status: Passed - CodeTree

--- 240903/금 채굴하기/gold-mining.py
+++ 240903/금 채굴하기/gold-mining.py
@@ -3,12 +3,21 @@ import sys
 input = sys.stdin.readline
 
 def get_gold_sum(r, c, K):
-    return sum([
-        board[i][j]
-        for i in range(n)
-        for j in range(n)
-        if abs(r - i) + abs(c - j) <= K
-    ])
+    gold_sum = 0
+    drs = [1, 1, -1, -1]
+    dcs = [-1, 1, 1, -1]
+
+    gold_sum += board[r][c]  # K = 0일 때 처리
+    for curr_k in range(1, K + 1):
+        curr_r, curr_c = r - curr_k, c
+        for dr, dc in zip(drs, dcs):
+            for step in range(curr_k):
+                if 0 <= curr_r < n and 0 <= curr_c < n:
+                    gold_sum += board[curr_r][curr_c]
+                curr_r = curr_r + dr
+                curr_c = curr_c + dc
+    
+    return gold_sum
 
 def get_area(K):
     return K * K + (K + 1) * (K + 1)
